550b9f830d2393252e514adacc35a7db9e2c8d9b
DC3PO <dc3po0@gmail.com>
2020-08-03 11:22:40 -0600

    Create About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the To-Do List App v1.0.0.  It is part of a 
+            React crash course.  I then styled it to my liking and
+            made sure it is formatted for all devices.  I have the 
+            pre-posted to-dos being pulled from https://jsonplaceholder.typicode.com/todos.</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;

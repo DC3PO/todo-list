b8835d3d69ad006ab1f6c36b13b4877d8f5062d4
DC3PO <dc3po0@gmail.com>
2020-08-03 11:30:09 -0600

    Add files via upload

---
+++ build/precache-manifest.926ef304d298dc475fbddf754826d926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0b84917f4d299c6d121fc079020ca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a297ff8454298f7e2654",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "c5788bc23c8b0e2c71a5",
+    "url": "/static/js/2.b31e0167.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b31e0167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a297ff8454298f7e2654",
+    "url": "/static/js/main.24c2c408.chunk.js"
+  },
+  {
+    "revision": "b51937f7bf637b46ab83",
+    "url": "/static/js/runtime-main.021fe4f4.js"
+  }
+]);

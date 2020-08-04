77c22db981df95ccd1d7870681077bec4b7261cf
Billy Hunt <billyhunt@navapbc.com>
2020-08-04 11:25:39 -0400

    code review

--- src/App.js
+++ src/App.js
@@ -1,65 +1,81 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import About from './components/pages/About';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
 // import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
+const todoEndpoint = "https://jsonplaceholder.typicode.com/todos"
 
 class App extends Component {
   state = {
-    todos: []
-  }
+    todos: [],
+  };
+
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => this.setState({ todos: res.data }))
+    axios
+      .get(`${todoEndpoint}?_limit=10`)
+      .then((res) => this.setState({ todos: res.data }));
   }
 
   // Toggle Complete
   markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-    }
-    return todo;
-    }) });
-  }
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
 
   // Delete Todo
   delTodo = (id) => {
-    axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
-      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id
-        !== id)] }));
-  }
+    axios
+      .delete(`${todoEndpoint}/${id}`)
+      .then((res) =>
+        this.setState({
+          todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+        })
+      );
+  };
 
   // Add Todo
   addTodo = (title) => {
-    axios.post('https://jsonplaceholder.typicode.com/todos', {
-      title,
-      completed: false
-    })
-      .then(res => this.setState({ todos: 
-      [...this.state.todos, res.data] }));
-  }
+    axios
+      .post(todoEndpoint, {
+        title,
+        completed: false,
+      })
+      .then((res) => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
 
   render() {
-    console.log(this.state.todos)
+    console.log(this.state.todos);
     return (
       <Router>
         <div className="App">
           <div className="container">
             <Header />
-            <Route exact path="/" render={props => (
-              <React.Fragment>
-                <AddTodo addTodo={this.addTodo} />
-                <Todos todos={this.state.todos} markComplete={this.markComplete} 
-                delTodo={this.delTodo} />
-              </React.Fragment> 
-            )} />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
             <Route path="/about" component={About} />
           </div>
         </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
-    getStyle = () => {
-       return {
-           background: '#C3E991',
-           padding: '10px',
-           borderBottom: '1px #75485E dotted',
-           textDecoration: this.props.todo.completed ?
-           'line-through' : 'none'
-       }
-    }
+  getStyle = () => {
+    return {
+      background: "#C3E991",
+      padding: "10px",
+      borderBottom: "1px #75485E dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
 
-    render() {
-      const { id, title } = this.props.todo;
-        return (
-            <div style={this.getStyle()}>
-              <p>
-                <input type="checkbox" onChange={this.props.markComplete.bind
-                (this, id)} /> {' '}
-                { title }
-                <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
-              </p>  
-            </div>
-        )
-    }
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 // PropTypes
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired,
-    markComplete: PropTypes.func.isRequired,
-    delTodo: PropTypes.func.isRequired
-  }
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
 
 const btnStyle = {
-  background: '#75485E',
-  color: '#fff',
-  border: 'none',
-  padding: '5px 10px',
-  borderRadius: '50%',
-  cursor: 'pointer',
-  float: 'right'
-}
+  background: "#75485E",
+  color: "#fff",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
+};
 
-export default TodoItem
+export default TodoItem;
